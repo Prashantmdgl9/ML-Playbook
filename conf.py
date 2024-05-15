257c3307d1acde7376cde6b7c79cb9c79c41dccf
Prashant Mudgal <Prashantmdgl9@users.noreply.github.com>
2024-05-15 17:55:04 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,4 +1,4 @@
-api_key = "sk-proj-77Z4ZOppLfsAe1aFv3soT3BlbkFJ3VEY9cHw1W4RnFWGiLrx"
+api_key = "test_WtGWTaB1rJPZBWOphqhAy46TE2oLgkZsd7vU7baK"
 model = "gpt-3.5-turbo"
 port = 5432
 db_password = '6-2RVDGOHifGjuaq4uWmRoQH9koMm5Eu'
